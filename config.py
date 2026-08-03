964b6da8d8d52cf1dc11358e84ca8893940be2ca
mohdafhamdev <mohammedafham790@gmail.com>
2026-08-02 22:59:30 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ["8941315572:AAEjayQGmFnch0b-zK1srvd5-0n2_ZaveQA"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 # Comma-separated list of admin Telegram user IDs, e.g. "123456789,987654321"
-ADMIN_IDS = [int(uid.strip()) for uid in os.environ.get("977205620, 8648725144", "").split(",") if uid.strip()]
+ADMIN_IDS = [int(uid.strip()) for uid in os.environ.get("ADMIN_IDS", "").split(",") if uid.strip()]
